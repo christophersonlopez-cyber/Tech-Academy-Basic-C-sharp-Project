9f44b08627c03b5724a5b94d9d6848bcfc398236
christopher son lopez <christophersonlopez@gmail.com>
2026-02-23 23:18:38 -0500

    this is my assignment

---
+++ Basic_C#_Programs/myConsoleProject/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+
+namespace myConsoleProject;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remember, we’re \"accounting\" on you!");
+
+        Console.ReadLine();
+    }
+}
