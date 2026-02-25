26a579df90f09c69f8488a668b6a78f0a4fb89c0
christopher son lopez <christophersonlopez@gmail.com>
2026-02-24 20:11:18 -0500

    add a comment

--- VariableandDataTYpe/Program.cs
+++ VariableandDataTYpe/Program.cs
@@ -19,27 +19,27 @@ class Program
        // Console.WriteLine("Your favorite number plus 5 is " + total);
 
 
-        bool istrue = true;
-        byte HOurs = 45;
-        sbyte minutes = -30;
+       // bool istrue = true;
+       // byte HOurs = 45;
+       // sbyte minutes = -30;
 
-        char symbol = '@';
-
-
-        decimal money = 100.50m;
-        float decimalnumber = 3.14f;
-        double bigdecimalnumber = 3.1415926535897932384626433832795;
-
-        short shortnumber = 32767;
-         string text = "This is a string";
+       // char symbol = '@';
 
 
-        int currentage = 30;
-        string age = currentage.ToString();
+        //decimal money = 100.50m;
+       // float decimalnumber = 3.14f;
+       //double bigdecimalnumber = 3.1415926535897932384626433832795;
+
+       // short shortnumber = 32767;
+        // string text = "This is a string";
 
 
-        bool israining = false;
-        string raining = Convert.ToString(israining);
-        Console.WriteLine("The Age is " + raining);
+       // int currentage = 30;
+       // string age = currentage.ToString();
+
+
+       // bool israining = false;
+      //  string raining = Convert.ToString(israining);
+       //  Console.WriteLine("The Age is " + raining);
     }
 }
