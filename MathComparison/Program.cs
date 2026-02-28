d756fa2a309433f65dcb56eb1e4f87138c57e650
christopher son lopez <christophersonlopez@gmail.com>
2026-02-27 22:32:49 -0500

    Create Program.cs

---
+++ MathComparison/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+
+public class Program
+{
+    public static void Main()
+    {
+        //int totalq = 10+2;
+        //int Total2 = 15+12;
+        // int combine = totalq + Total2;
+        // Console.WriteLine("The total is: " + combine);
+        // Console.ReadLine();
+
+        //  int diference = 10 - 2;
+        //  Console.WriteLine("The diference is: " + diference.ToString());
+        //Console.ReadLine();
+
+        // int product = 10 * 2;
+        // Console.WriteLine("The product is: " + product);
+        //Console.ReadLine();
+
+        //double quotient = 10.0 / 7.0;
+        // Console.WriteLine("The quotient is: " + quotient);
+        //Console.ReadLine();
+
+        int remainder = 10 % 3;
+        Console.WriteLine("The remainder is: " + remainder);
+        Console.ReadLine();
+    }
+}
