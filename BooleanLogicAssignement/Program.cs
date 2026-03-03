a9d750671442b53007f559d238ad9612810aa6ae
christopher son lopez <christophersonlopez@gmail.com>
2026-03-02 23:22:55 -0500

    update a code

--- BooleanLogicAssignement/Program.cs
+++ BooleanLogicAssignement/Program.cs
@@ -23,17 +23,10 @@ bool hasDUI = Convert.ToBoolean(Console.ReadLine());
 Console.WriteLine("How many speeding tickets do you have?");
 int speedingTickets = Convert.ToInt32(Console.ReadLine());
 
-//this line of code will check if the user is eligible for car insurance based on the criteria of being over 15 years old, not having a DUI, and having 3 or fewer speeding tickets
-if (age>15&&hasDUI==false&&speedingTickets<=3) {
-    //this line of code will print "You are eligible for car insurance." to the console if the user meets the criteria
-    Console.WriteLine("You are eligible for car insurance.");
-}
-else {
-    //this line of code will print "You are not eligible for car insurance." to the console if the user does not meet the criteria
-    Console.WriteLine("You are not eligible for car insurance.");
-}
-
-
+Console.WriteLine("\n");
+Console.WriteLine("Qualified for car insurance?");
+bool results = (age > 15) && (hasDUI == false) && (speedingTickets <= 3);
+Console.WriteLine(results);
 
 
 
