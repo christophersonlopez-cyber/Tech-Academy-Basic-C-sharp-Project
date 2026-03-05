3b98c8f13b7819d31f16d3c6c3e413cfb117af15
christopher son lopez <christophersonlopez@gmail.com>
2026-03-04 22:20:19 -0500

    Create Program.cs

---
+++ TernaryOperatorchallenge/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("This is ternary operator\n");
+        int num1 = 20;
+        int num2 = 10;
+        string result = num1 < num2 ? $"The {num1} is greather than {num2}" : $"The {num1} is greater than {num2}";
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+}
