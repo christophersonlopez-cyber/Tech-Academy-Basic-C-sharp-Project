d9ca7019a98a37397cc6bad1c0c2d55c1c2d63c6
christopher son lopez <christophersonlopez@gmail.com>
2026-03-06 23:37:13 -0500

    Create Program.cs

---
+++ DoWHile_WhileLoop/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Diagnostics;
+
+
+public class Program
+{
+    public static void Main()
+    {
+
+        //this is a simple while loop that prints the numbers from 1 to 5
+        Console.WriteLine("This is a while loop that prints the numbers from 1 to 5:\n");
+        int num = 1;
+
+        while (num <= 5)
+        {
+            Console.WriteLine(num);
+            num++;
+        }
+        Console.WriteLine("\n\n");
+        //this is a simple do-while loop that prints the numbers from 1 to 10
+
+        Console.WriteLine("This is a do-while loop that prints the numbers from 1 to 10:\n");
+        int num2 = 1;
+
+        do
+        {
+            Console.WriteLine(num2);
+            num2++;
+        }
+        while (num2<=10);
+    }
+}
