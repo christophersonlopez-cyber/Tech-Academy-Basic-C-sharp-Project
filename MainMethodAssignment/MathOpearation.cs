923085cd8838d69cfef936c4eb6a15eb86a58c04
christopher son lopez <christophersonlopez@gmail.com>
2026-04-01 00:19:49 -0400

    this is my class method for my main assignment

---
+++ MainMethodAssignment/MathOpearation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    internal class MathOpearation
+    {
+        public  int addtion(int num) {
+            return num +10;
+        }
+        public int multiply(decimal num)
+        {
+            int num2 = Convert.ToInt32(num);
+            return num2 * 5;
+        }
+        public int Subtract(string num)
+        {
+            int num2 = Convert.ToInt32(num);
+            return num2 - 5;
+        }
+    }
+}
