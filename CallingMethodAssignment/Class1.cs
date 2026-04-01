8793c6976d06fd743ce3909c420552306d110065
christopher son lopez <christophersonlopez@gmail.com>
2026-04-01 00:20:20 -0400

    i create a class1 method

---
+++ CallingMethodAssignment/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodAssignment
+{
+
+
+    internal class Class1
+    {
+        int num1, num2;
+        //this is for my addtion method
+        public int AddNUmber(int num1,int num2)
+        {
+        
+            return num1+num2;
+        }
+
+        public int multiplyNumber(int num1,int num2)
+        {
+            return num1*num2;
+        }
+
+        public int subtractNumber(int num1,int num2)
+        {
+            return num1-num2;
+        }
+
+
+    }
+}
