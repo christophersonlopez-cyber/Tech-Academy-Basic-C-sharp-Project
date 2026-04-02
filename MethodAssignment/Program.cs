b49c675c4e522e95c1c5fba25b5687f4cb1c5244
christopher son lopez <christophersonlopez@gmail.com>
2026-04-01 22:27:47 -0400

    Create Program.cs

---
+++ MethodAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using MethodAssignment;
+using System;
+
+
+
+public class Program
+{
+    public static void Main()
+    {
+        mathop1 Addmath=new mathop1();
+        Console.WriteLine("Input first Number");
+        int num=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input second Number or press Enter");
+
+
+		try
+		{
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int result = Addmath.Add(num, num2);
+            Console.WriteLine($"The sum of {num} and {num2} is {result}");
+        }
+		catch (Exception)
+		{
+
+			Console.WriteLine("The sum is: " + (num));
+        }
+
+    }
+}
