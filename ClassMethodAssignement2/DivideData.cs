946376f273ec56a6fc941430238a86f4276c999d
christopher son lopez <christophersonlopez@gmail.com>
2026-04-02 18:31:14 -0400

    this my assignment

---
+++ ClassMethodAssignement2/DivideData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignement2
+{
+    internal class DivideData
+    {
+        //thuis l;ine i used a void so that why i created a object in main program
+        public void MathOperation(int num1) {
+            int result = num1 / 2;
+            Console.WriteLine($"The Input is {num1} divided by 2 is "+result+"\n\n");
+        
+        }
+        //this line i used static to call the class and method directly
+        public static void MathOperation(double input1, out double result) { 
+            //this line i use a output parameter out to hold and return the value {
+            result = input1 / 3;
+        
+        }
+    }
+}
---
+++ ClassMethodAssignement2/Program.cs
@@ -0,0 +1,26 @@
+﻿using ClassMethodAssignement2;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+
+public class Program {
+    public static void Main(string[] args)
+    {
+        //this line i use a void method so  i call the class DivideData and declare a new object
+        DivideData divideData = new DivideData();
+        Console.WriteLine("Input a number");
+        int input=Convert.ToInt32(Console.ReadLine());
+
+        
+      
+        divideData.MathOperation(input);
+        //this line i used a static so that why i call direct the class method
+        double result;
+        DivideData.MathOperation(input,out result);
+        Console.WriteLine($"The input {input} divided by 3 is "+result);
+
+    }
+
+
+
+}
