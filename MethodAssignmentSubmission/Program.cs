3d22f8faffccac8d658cda8f338ef61d76782fa4
christopher son lopez <christophersonlopez@gmail.com>
2026-04-01 23:36:29 -0400

    this is my assignment

---
+++ MethodAssignmentSubmission/Program.cs
@@ -0,0 +1,22 @@
+﻿using MethodAssignmentSubmission;
+using System;
+
+
+public class Program {
+
+    public static void Main(){
+        //this line i call the class name SimpleAdd then a
+        //create a object Addnumber to call the method in the class
+        SimpleAdd Addnumber = new SimpleAdd();
+
+
+        //then this line i call the method name addnumbers
+        //to add the firstnumber and dislay thew second number in the screen
+            Addnumber.addnumbers(5, 15);
+        //then this line icall the parameter name and assigned a value of num1 and num2 variable
+
+        Addnumber.addnumbers(num1: 10, num2: 15);
+
+}
+
+}
---
+++ MethodAssignmentSubmission/SimpleAdd.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignmentSubmission
+{
+    internal class SimpleAdd
+
+
+    {
+        //this line of code i creat a method a method name called a addnumbers \
+        //and this line a create a two integer parameter call a num1 and num2
+        public void addnumbers(int num1,int num2) {
+            //this line i create a variable to hold a result of adding a number 
+            //and i used a integer datatype
+            int result = num1 + 10;
+            //this line i print my name in screen but this not include in the assignment but i try this one to make it better
+            Console.WriteLine("Hi im Christopher this is my assignment \n\n");
+            //this line i print the output of adding a numbers and i use a $ sign to put a variable inside if the statement
+             Console.WriteLine($"The result of adding {num1} and 10 is "+result+"\n\n");//this line \n is for a new line
+           //this line is to displkay the second number
+            Console.WriteLine("This is the second number "+num2+"\n");
+   
+   
+
+
+    
+        
+        }
+    }
+}
