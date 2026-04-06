cd80c95efb4fbaed1ee0c08f2bcce165e9d15f16
christopher son lopez <christophersonlopez@gmail.com>
2026-04-05 23:18:12 -0400

    This is my assignment

---
+++ OperatorAssignment/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorAssignment
+{
+    public class Employee
+    {
+        //this class has three properties ID, Firstname and Lastname
+        public int ID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        //this line i overload the equality operator to compare the ID of two
+        //employee objects and return true if they are equal and false if they are not equal
+        public static bool operator == (Employee emp1, Employee emp2)
+        {
+        return emp1.ID == emp2.ID;
+        }
+        //this line i overload the inequality operator to compare the ID of two employee objects
+        //and return true if they are not equal and false if they are equal
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return !(emp1.ID == emp2.ID);
+        }
+    }
+}
---
+++ OperatorAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using OperatorAssignment;
+using System;   
+
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //thias line i defined two employee objects with the same ID but different first names and last names
+        Employee emp1 = new Employee { ID = 1, Firstname = "Christopheer", Lastname = "Lopez" };
+        Employee emp2 = new Employee { ID = 1, Firstname = "Jane", Lastname = "Lopez" };
+        //then this line io declare a boolean data type variable to compare the Id of the employee true or false
+        bool areEqual=emp1==emp2;
+        //this l;ine i print the information of the two employee objects and the result of the comparison of their IDs
+        Console.WriteLine($"Employee 1 Information\n\nID: {emp1.ID}\nFirstname: {emp1.Firstname}\nLastname: {emp1.Lastname}");
+        Console.WriteLine($"\nEmployee 2 Information\n\nID: {emp2.ID}\nFirstname: {emp2.Firstname}\nLastname: {emp2.Lastname}");
+       
+        
+        Console.WriteLine($"Are Employee 1 Id  and Employee 2 ID are equal? {areEqual}");
+
+        //this line i declare a boolean data type variable to compare the Id of the employee not equal true or false
+        bool areNotEqual =emp1!=emp2;
+
+        Console.WriteLine($"Are Employee 1 Id  and Employee 2 ID are not equal? {areNotEqual}");
+    }
+}
