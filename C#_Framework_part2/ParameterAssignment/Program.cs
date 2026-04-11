25e1fcd013a4a53b433186bcd86103b7afa43797
christopher son lopez <christophersonlopez@gmail.com>
2026-04-10 22:29:53 -0400

    i create a generic

---
+++ C#_Framework_part2/ParameterAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using ParameterAssignment;
+using System;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        Employee<string> employee = new Employee<string>();
+        employee.things = new List<string>() { "Christopher", "Zander", "Zairine", "Rizen" };
+        Employee<int> age = new Employee<int>();
+        age.things = new List<int>() { 30, 10, 28, 17 };
+        for (int i = 0; i < employee.things.Count; i++)
+        {
+            Console.WriteLine("My Name is : " + employee.things[i]);
+           
+        }
+        for (int i = 0; i < age.things.Count; i++)
+        {
+            Console.WriteLine("My Age is : " + age.things[i]);
+        }
+
+        for (int i = 0; i < employee.things.Count; i++)
+        {
+            Console.WriteLine("My Name is : " + employee.things[i] + " and My Age is : " + age.things[i]);
+        }
+    }
+}
---
+++ C#_Framework_part2/ParameterAssignment/employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameterAssignment
+{
+    //this line i create a class employee and i use a generic type T to make it flexible
+    public class Employee<T>
+    {
+        //this line i create  a property things which is a list of typet to store a data
+        public List<T> things { get; set; }
+    }
+}
