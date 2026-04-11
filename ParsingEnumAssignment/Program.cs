aed2d87a811e55c1ce8913eac733f14fd30e249e
christopher son lopez <christophersonlopez@gmail.com>
2026-04-10 23:27:22 -0400

    create  a parsing enum assignment

---
+++ ParsingEnumAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+public enum DaysOfWeek
+{
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        bool continueInput = true;
+        while (continueInput)
+        {
+            try
+            {
+                Console.WriteLine("Input a day of the week:");
+                string inputuser = Console.ReadLine();
+                DaysOfWeek day = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), inputuser, true);
+                Console.WriteLine($"Today is: {day}");
+                continueInput = false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Please enter an actual day of the week.");
+
+            }
+
+        }
+    }
+}
