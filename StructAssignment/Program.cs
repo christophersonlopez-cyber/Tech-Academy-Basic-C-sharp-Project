175be89fa7ebed6bcd58e63f58e3735f3854f65b
christopher son lopez <christophersonlopez@gmail.com>
2026-04-12 22:28:41 -0400

    Create Program.cs

---
+++ StructAssignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+struct Number { 
+    public decimal amount;  
+
+}
+public class Program
+{
+    public static void Main()
+    {
+        Number num1 = new Number {amount= 100.50m };
+
+        Console.WriteLine(num1.amount);
+    }
+}
