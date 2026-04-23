bdf3c862e22316828812039476ca97a6b839f99d
christopher son lopez <christophersonlopez@gmail.com>
2026-04-23 00:20:56 -0400

    This is my Assignment

---
+++ CarInsurance/Controllers/InsureeController.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarInsurance.Models;
+
+namespace CarInsurance.Controllers
+{
+    public class InsureeController : Controller
+    {
+        private InsuranceEntities db = new InsuranceEntities();
+
+        // GET: Insuree
+        public ActionResult Index()
+        {
+            return View(db.Insurees.ToList());
+        }
+
+        // GET: Insuree/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Insuree/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Firstname,Lastname,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                double quote = 50;
+
+                int age = DateTime.Now.Year - insuree.DateOfBirth.Year;
+                if (insuree.DateOfBirth > DateTime.Now.AddYears(-age)) 
+{
+                    age--;
+                }
+                if (age <= 18)
+                {
+
+                    quote += 100;
+                }
+                else if (age <= 25)
+                {
+                    quote += 50;
+                }
+                else
+                {
+                    quote += 25;
+                }
+
+
+                if (insuree.CarYear < 2000)
+                {
+                    quote += 25;
+
+                }
+                 if (insuree.CarYear > 2015)
+                {
+                    quote += 25;
+                }
+
+                if (insuree.CarMake.ToLower() == "porsche")
+                {
+                    quote += 25;
+                    if (insuree.CarModel.ToLower ()== "911 carrera")
+                    {
+
+                        quote += 25;
+                    }
+                
+                
+                }
+                quote += insuree.SpeedingTickets * 10;
+                if (insuree.DUI)
+                {
+                    quote *= 1.25;
+                }
+                if (insuree.CoverageType)
+                {
+                    quote *= 1.5;
+                
+                }
+                    insuree.Quote = Convert.ToDecimal(quote);
+
+                    db.Insurees.Add(insuree);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(insuree);
+        }
+
+        // GET: Insuree/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+        
+            return View(insuree);
+        }
+
+        // POST: Insuree/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Firstname,Lastname,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                insuree.Quote = CalculateQuote(insuree);
+
+                db.Entry(insuree).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Insuree insuree = db.Insurees.Find(id);
+            db.Insurees.Remove(insuree);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult Admin() {
+        var data=db.Insurees.ToList();
+        return View(data);
+        }
+        private decimal CalculateQuote(Insuree insuree)
+        {
+            decimal quote = 50;
+
+            int age = DateTime.Now.Year - insuree.DateOfBirth.Year;
+            if (insuree.DateOfBirth > DateTime.Now.AddYears(-age)) age--;
+
+            if (age <= 18) quote += 100;
+            else if (age <= 25) quote += 50;
+            else quote += 25;
+
+            if (insuree.CarYear < 2000) quote += 25;
+            if (insuree.CarYear > 2015) quote += 25;
+
+            if (insuree.CarMake.ToLower() == "porsche")
+            {
+                quote += 25;
+                if (insuree.CarModel.ToLower() == "911 carrera")
+                    quote += 25;
+            }
+
+            quote += insuree.SpeedingTickets * 10;
+
+            if (insuree.DUI) quote *= 1.25m;
+
+            if (insuree.CoverageType) quote *= 1.5m;
+
+            return quote;
+        }
+
+
+    }
+}
