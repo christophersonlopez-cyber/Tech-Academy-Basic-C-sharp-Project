77f7ef453b4b30a35f781ce24383234adf62e45a
christopher son lopez <christophersonlopez@gmail.com>
2026-04-23 22:57:36 -0400

    challenge

---
+++ ChallengeAssignment/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+using System.Diagnostics;
+using ChallengeAssignment.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChallengeAssignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+       
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new Person
+            {
+
+                currenttime = DateTime.Now
+            };
+            return View(model);
+        }
+      
+        
+
+        [HttpPost]
+        public IActionResult Index(Person model)
+        { 
+        return View(model);
+        
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ChallengeAssignment/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace ChallengeAssignment.Models
+{
+    public class Person
+    {   public DateTime currenttime { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Middlename { get; set; }
+        public string message { get; set; }
+        public string fullname() {
+            string full= Firstname + " " + Middlename + " " + Lastname;
+            return (full);
+        }
+    }
+}
